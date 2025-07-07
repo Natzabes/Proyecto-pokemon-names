40de2d8304c34eb7a3dd1c2dd84bc2e7d4a9d100
Natzabes <sr786382@gmail.com>
2025-07-07 13:40:05 -0400

    Estilizacion de falta de resultado

--- src/PokemonFetcher.jsx
+++ src/PokemonFetcher.jsx
@@ -2,6 +2,7 @@ import { useState, useEffect } from "react";
 import './PokemonFetcher.css';
 
 function PokemonFetcher() {
+    const [mensaje, setMensaje] = useState("");
     const [pokemon, setPokemon] = useState([]);
     const [inputValue, setInput] = useState("");
     const [cargando, setCargando] = useState(false);
@@ -12,16 +13,13 @@ function PokemonFetcher() {
         e.target.setCustomValidity('') //Reinicia mensaje de validez cuando usuario escribe, ya que no se sabe si es valido aún o no mientras escribe.
     }
 
-    /* useEffect(() => {
-        
-    }, []); */
-
-    if(cargando) {
-        return <p>Cargando Pokémon...</p>;
-    }
-    if(error) {
-        return <p>Error: {error}</p>;
-    }
+    useEffect(() => {
+        if(cargando === true) {
+            setMensaje(`Cargando Pokémon...`);
+        } else if(error) {
+            setMensaje(`Error: ${error}`);
+        } else setMensaje(`Resultados: `);
+    }, [cargando, error, mensaje]);
 
     const handleSubmit = async (e) => {
         e.preventDefault();
@@ -43,23 +41,28 @@ function PokemonFetcher() {
 
             setPokemon([fetchedPokemon]);
 
-        } catch(err) {setError(err.message);
-        } finally {setCargando(false);}
+        } catch(err) {
+            setError(err.message);
+            setPokemon([]);
+        } finally {
+            setCargando(false);
+        }
     }
 
     return (
         <div className="pokemon-container">
-            <h2>Busca Pokémon: </h2>
+            <h2>Busca un Pokémon: </h2>
             <form onSubmit={handleSubmit}>
                 <input type="text" placeholder="Pokémon" name="query" value={inputValue} onChange={(e) => registraCambioValor(e)} minLength={3} maxLength={30} onInvalid={(e) => e.target.setCustomValidity("Ingrese de 3-30 carácteres.")}/>
                 <button>Buscar</button>
+                <p>{mensaje}</p>
             </form>
             <div className="pokemon-list">
                 {pokemon.map(pokemon => (
                     <div key={pokemon.id} className="pokemon-card">
                         <h3>{pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1)}</h3>
                         <img src={pokemon.image} alt={pokemon.name}/>
-                        <p>Types: {pokemon.types}</p>
+                        <p>Type(s): {pokemon.types}</p>
                     </div>
                 ))}
             </div>
